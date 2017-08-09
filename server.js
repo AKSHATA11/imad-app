2110ae742174a50f3449fbbb7414bd15e23f664a
AKSHATA11 <30820321+AKSHATA11@users.noreply.github.com>
2017-08-09 11:02:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile('Article one requested and will be served');
+  res.send('Article one requested and will be served');
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
